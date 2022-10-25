c106a547e848f69b97358b87f6b835a96ccc0827
Alan M Oliveira <alanmartins789@gmail.com>
2022-10-24 21:25:25 -0300

    first commit

---
+++ comments.rb
@@ -0,0 +1,13 @@
+# hashtag used for Comments
+#-------------------------------
+=begin
+Comments in block  
+=end
+#--------------------------------
+#puts is used for print in console screen
+#-------------------------------------------
+#gets is used for read value in console screen
+#-------------------------------------------
+#ruby operadores_entrada.rb used for run the console
+#----------------------------------------------------
+
---
+++ teste.rb
@@ -0,0 +1,9 @@
+puts "Digite o seu Primeiro nome: "
+fistName = gets
+puts "Digite o Sobrenome: "
+lastName = gets
+
+fullName = fistName + lastName
+
+puts " O seu nome é "+fullName 
+#puts "Seu nome é " "#{fistName} #{lastName}"
